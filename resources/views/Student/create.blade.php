df758cb1043122d3ec7d1bb1b0acb89c3b07a355
KevinJAM2314 <kevin.arroyomora@ucr.ac.cr>
2023-06-08 11:10:18 -0600

    Errors changed

--- resources/views/Student/create.blade.php
+++ resources/views/Student/create.blade.php
@@ -4,40 +4,27 @@
 
 @section('content')
   <h2>{{ __('students.form.create', ['attribute' => __('students.student')]) }} </h2>
-
+  @foreach($errors->all() as $error)
+    <br>
+    <small style="color: red;">*{{ $error }}</small>
+  @endforeach
   <form action="/students" method="POST">
     @csrf
     <div class="mb-3">
       <label for="First_name" class="form-label">{{ __('students.attributes.first_name') }}</label>
       <input id="first_name" name="first_name" type="text" class="form-control" tabindex="1" value="{{ old('first_name') }}">
-
-      @error('first_name')
-        <br>
-        <small style="color: red;">*{{ $message }}</small>
-        <br>
-      @enderror
     </div>
 
     <div class="mb-3">
       <label for="" class="form-label"> {{ __('students.attributes.last_name') }}</label>
       <input id="last_name" name="last_name" type="text" class="form-control" tabindex="2" value="{{ old('last_name') }}">
 
-      @error('last_name')
-        <br>
-        <small style="color: red;">*{{ $message }}</small>
-        <br>
-      @enderror
-
     </div>
+
     <div class="mb-3">
       <label for="" class="form-label">{{ __('students.attributes.identification') }}</label>
       <input id="identification" name="identification" type="text" class="form-control" tabindex="3" value="{{ old('identification') }}">
 
-      @error('identification')
-        <br>
-        <small style="color: red;">*{{ $message }}</small>
-        <br>
-      @enderror
     </div>
     <a href="/students" class="btn btn-secondary" tabindex="5">{{__('pagination.previous')}}</a>
     <button type="submit" class="btn btn-primary" name="submit_button" tabindex="4">{{__('pagination.save')}}</button>
--- resources/views/Student/edit.blade.php
+++ resources/views/Student/edit.blade.php
@@ -2,36 +2,24 @@
 
 @section('content')
 <h2>{{ __('students.form.edit', ['attribute' => __('students.student')]) }} </h2>
-
+@foreach($errors->all() as $error)
+    <br>
+    <small style="color: red;">*{{ $error }}</small>
+@endforeach
 <form action="/students/{{$student->id}}" method="POST">
     @csrf
     @method('PUT')
   <div class="mb-3">
     <label for="" class="form-label">{{ __('students.attributes.first_name') }}</label>
     <input id="first_name" name="first_name" type="text" class="form-control" value="{{$student->first_name}}">
-    @error('first_name')
-      <br>
-      <small style="color: red;">*{{ $message }}</small>
-      <br>
-    @enderror
   </div>
   <div class="mb-3">
     <label for="" class="form-label">{{ __('students.attributes.last_name') }}</label>
     <input id="last_name" name="last_name" type="text" class="form-control" value="{{$student->last_name}}">
-    @error('last_name')
-      <br>
-      <small style="color: red;">*{{ $message }}</small>
-      <br>
-    @enderror
   </div>
   <div class="mb-3">
     <label for="" class="form-label">{{ __('students.attributes.identification') }}</label>
     <input id="identification" name="identification" type="text" class="form-control" value="{{$student->identification}}">
-    @error('identification')
-      <br>
-      <small style="color: red;">*{{ $message }}</small>
-      <br>
-    @enderror
   </div>
   <a href="/students" class="btn btn-secondary" tabindex="5">{{__('pagination.previous')}}</a>
   <button type="submit" class="btn btn-primary" tabindex="4">{{__('pagination.save')}}</button>
